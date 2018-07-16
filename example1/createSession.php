e15ed9f6e51de1d147b4f0e910d51f745476866a
niteeshbhat <niteeshbhathm@gmail.com>
2018-07-16 10:16:41 +0200

    Updates from waqar

--- example1/createSession.php
+++ example1/createSession.php
@@ -1,21 +1,13 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Helper method to create folder
  */
 $sessionId=$_POST['sessionId'];
 
 mkdir($sessionId);
-//chmod("temp/".$sessionId."/", 0777);
 exec("sudo chmod -R 0777 ".$sessionId."/");
-
 copy("track.xml",$sessionId."/track.xml"); 
-
-
 echo "Session created "."$sessionId";
-
-
 
 ?> 
--- example1/decoder.php
+++ example1/decoder.php
@@ -1,58 +1,68 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 $sessionId=$_POST['sessionId'];
+$trackFilename = $sessionId.'/track.xml';
 
-$load=simplexml_load_file($sessionId.'/track.xml');
+//Simply read the XML track file in DOM
+$trackData = getTrackDataFromTrackFile($trackFilename);
 
-if($load !== FALSE){
-    $dom_abs = dom_import_simplexml($load);
-    $abs = new DOMDocument('1.0');
-    $dom_abs = $abs->importNode($dom_abs, true);
-        
-    $dom_abs = $abs->appendChild($dom_abs);
-    
-    $trackData = $abs->getElementsByTagName('trackData')->item(0);
-}
-else
-    echo "Track data not found";
+//Get some relevant parameters
+$sampleCount=$trackData->getElementsByTagName('sampleCount')->item(0)->nodeValue;
 
-$init=$trackData->getElementsByTagName('init')->item(0);
-$template=$init->getElementsByTagName('template')->item(0)->nodeValue;
-$sampleCount=$init->getElementsByTagName('sampleCount')->item(0)->nodeValue;
+//Extract the segment template
+$template=$trackData->getElementsByTagName('template')->item(0)->nodeValue;
 
-//As xml load removes html tags, init page is not possible to save this way
-//Hence other file read option is used to save init webpage to a file.
-$fullFile= file_get_contents($sessionId.'/track.xml');
-$fullFile=str_replace("&lt;", "<", $fullFile);
-$fullFile=str_replace("&amp;", "&", $fullFile);
-//$fullFile=str_replace("temp/sample", "temp/".$sessionId."/sample", $fullFile);
+//Extract the initial webpage, save
+$initHTML=$trackData->getElementsByTagName('html')->item(0);
+$initalHTMLFile = $sessionId.'/init.html';
+file_put_contents($initalHTMLFile,$trackData->saveXML($initHTML));
+replaceSpecialStrings($initalHTMLFile);//Replace the &lt; etc. in file
 
-$startPos=strpos($fullFile, "<html>");
-$endPos=strpos($fullFile,"</html>");
-
-$fileHandle=fopen($sessionId."/init.html","w");
-fwrite($fileHandle,substr($fullFile,$startPos,$endPos-$startPos+1+6));//To include till end of </html>
-fclose($fileHandle);
-
-$sampleNum=3;//As child node index 3 has the first sample.x
-for($i=1;$i<=(int)$sampleCount;$i++)
+/*Extract (decode) all samples at their decode times and write to server*/
+for($sampleNumber=1;$sampleNumber<=(int)($sampleCount);$sampleNumber+=1)
 {
-    $sample=$trackData->childNodes->item($sampleNum);
-    $decodeDur=$sample->getAttribute('decodeDur');
-    $script=$sample->getElementsByTagName('script')->item(0);
-    $fileHandle=fopen($sessionId.'/sample'.$i.'.x',"w");
-    fwrite($fileHandle,$decodeDur."\n");
-    fwrite($fileHandle,$script->nodeValue);
-    fclose($fileHandle);
-    $sampleNum+=2;
+    $sample=$trackData->documentElement->childNodes->item(($sampleNumber*2)+1); //Indexing and addressing of child node caters for extra "text" nodes
+    $sampleFileName=$sessionId . '/' . str_replace('$Number$', strval($sampleNumber), $template); //Create sample file name from template
+    file_put_contents($sampleFileName,$trackData->saveXML($sample->firstChild->nextSibling));
+    replaceSpecialStrings($sampleFileName);//Replace the &lt; etc. in file
+    
+    //Extract the decode duration of this sample, sleep the duration before decoding next sample.
+    $decodeDur=$sample->firstChild->nextSibling->getAttribute('decodeDur');
     sleep($decodeDur);
-
 }
+    
 echo "Decoding completed";
+
+/*Other helper methods*/
+function getTrackDataFromTrackFile($fileName)
+{
+    $load=simplexml_load_file($fileName);
+
+    if($load !== FALSE){
+        $dom_abs = dom_import_simplexml($load);
+        $abs = new DOMDocument('1.0');
+        $dom_abs = $abs->importNode($dom_abs, true); //create dom element to contain mpd 
+        $dom_abs = $abs->appendChild($dom_abs);
+        $trackData = $abs;
+    }
+    else
+        echo "Track data not found";
+    
+    return $abs;
+}
+
+function replaceSpecialStrings($fileName)
+{
+    //read the entire string
+    $str=file_get_contents($fileName);
+
+    //replace something in the file string
+    $str=str_replace("&lt;", "<", $str);
+    $str=str_replace("&gt;", ">", $str);
+    $str=str_replace("&amp;", "&", $str);
+
+    //write the entire string
+    file_put_contents($fileName, $str);
+}
 
 ?>  
